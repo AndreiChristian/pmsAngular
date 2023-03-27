242e1ffec86246d921d909aea0f278729f880a62
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-03-27 12:03:51 +0200

    starting the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { SidenavComponent } from './UI/sidenav/sidenav.component';
+import { FooterComponent } from './UI/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SidenavComponent, FooterComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [MaterialModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
